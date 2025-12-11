e328fd6c6770db46f3176074b4ed32b54a4ba687
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-12-11 22:13:52 +0545

    simple and advance qrcode program

---
+++ advance_qrcode_generate.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version= 1, 
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size= 20,
+                   border= 4,)
+qr.add_data("Hello! How are you ? ")
+qr.make(fit=True)
+img = qr.make_image(fill_color = "red", back_color = "yellow")
+img.save("text.png")
---
+++ simple_qrcode_generate.py
@@ -0,0 +1,7 @@
+import qrcode as qr
+
+image = qr.make("hello")
+image.save("taylo_song.png")
+
+
+
